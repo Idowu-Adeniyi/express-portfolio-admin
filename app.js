d5e639db48d78285529972c50a69da39992f8e96
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-04-17 19:51:11 -0400

    modified upload function

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-//app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB Connection
 mongoose
